7f05697fe13a399d34bd424dbfa701ce306c64e8
Jakub Rożek <jakub@stoplight.io>
2020-11-26 01:21:09 +0100

    Handle references that are not moved under custom root

--- test/specs/custom-bundling-roots/custom-roots.spec.js
+++ test/specs/custom-bundling-roots/custom-roots.spec.js
@@ -1,9 +1,11 @@
+/* eslint-disable camelcase */
 "use strict";
 
 const { expect } = require("chai");
 const $RefParser = require("../../..");
 const path = require("../../utils/path");
 const nock = require("nock");
+const { getDefaultsForOldJsonSchema } = require("../../../lib/bundle/defaults");
 const { getDefaultsForOAS2, getDefaultsForOAS3 } = require("../../../lib/bundle/defaults");
 
 describe("Custom bundling roots", () => {
@@ -100,6 +102,128 @@ describe("Custom bundling roots", () => {
           ]
         }
       },
+    });
+  });
+
+  it("should handle $refs whose parents were remapped", async () => {
+    nock("http://localhost:8080")
+      .get("/api/nodes.raw/")
+      .query({
+        srn: "gh/stoplightio/test/Book.v1.yaml"
+      })
+      .reply(200, {
+        properties: {
+          author: {
+            $ref: "#/definitions/Book_Author"
+          },
+          publisher: {
+            properties: {
+              city: {
+                $ref: "#/definitions/City"
+              }
+            }
+          }
+        },
+        definitions: {
+          City: {
+            properties: {
+              street: {
+                type: "string"
+              }
+            }
+          },
+          Book_Author: {
+            properties: {
+              name: {
+                type: "string"
+              },
+              contact: {
+                properties: {
+                  name: {
+                    $ref: "#/definitions/Book_Author/properties/name"
+                  },
+                  address: {
+                    street: {
+                      $ref: "#/definitions/City/properties/street"
+                    },
+                  }
+                }
+              }
+            }
+          }
+        },
+      });
+
+    const model = {
+      properties: {
+        id: {
+          type: "string"
+        },
+        book: {
+          $ref: "http://localhost:8080/api/nodes.raw/?srn=gh/stoplightio/test/Book.v1.yaml"
+        }
+      }
+    };
+
+    let parser = new $RefParser();
+
+    const schema = await parser.bundle(model, {
+      bundle: getDefaultsForOldJsonSchema(),
+    });
+
+    expect(schema).to.equal(parser.schema);
+    expect(schema).to.deep.equal({
+      definitions: {
+        "Book.v1": {
+          definitions: {},
+          properties: {
+            author: {
+              $ref: "#/definitions/Book.v1_Book_Author"
+            },
+            publisher: {
+              properties: {
+                city: {
+                  $ref: "#/definitions/Book.v1_City"
+                }
+              }
+            }
+          }
+        },
+        "Book.v1_Book_Author": {
+          properties: {
+            contact: {
+              properties: {
+                address: {
+                  street: {
+                    $ref: "#/definitions/Book.v1_City/properties/street"
+                  }
+                },
+                name: {
+                  $ref: "#/definitions/Book.v1_Book_Author/properties/name"
+                }
+              }
+            },
+            name: {
+              type: "string"
+            }
+          }
+        },
+        "Book.v1_City": {
+          properties: {
+            street: {
+              type: "string"
+            }
+          }
+        },
+      },
+      properties: {
+        book: {
+          $ref: "#/definitions/Book.v1"
+        },
+        id: {
+          type: "string"
+        }
+      }
     });
   });
 });
