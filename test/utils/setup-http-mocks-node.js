baccf6cf57b956666f0f4c5df0582063050d0c68
Jakub Rożek <jakub@stoplight.io>
2021-01-07 19:54:36 +0100

    Setup CircleCI

---
+++ test/utils/setup-http-mocks-node.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const nock = require("nock");
+const { URL } = require("url");
+
+afterEach(() => {
+  nock.cleanAll();
+});
+
+module.exports = function (mocks) {
+  for (const [url, body] of Object.entries(mocks)) {
+    const { origin, pathname, searchParams } = new URL(url);
+
+    const query = Object.fromEntries(searchParams.entries());
+
+    let scope = nock(origin)
+      .persist(true);
+
+    if (Object.keys(query).length > 0) {
+      scope = scope.get(pathname.replace(/\/$/, "") + "/").query(query);
+    }
+    else {
+      scope = scope.get(pathname);
+    }
+
+    scope .reply(200, body);
+  }
+};
